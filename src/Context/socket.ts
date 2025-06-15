c33d7ff539d6de849ea1ca7e76375b5566772899
Lemon-zeniye <endalklemon.abate@gmail.com>
2025-06-15 17:02:25 +0300

    init

--- src/Context/socket.ts
+++
@@ -1,9 +0,0 @@
-// src/socket.ts
-import { io } from "socket.io-client";
-
-const socket = io("http://194.5.159.228:3002/", {
-  transports: ["websocket"],
-  autoConnect: false,
-});
-
-export default socket;
