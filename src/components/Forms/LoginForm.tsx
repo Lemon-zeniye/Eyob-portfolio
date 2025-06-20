bfa8a448183b538ab750d4ba189c2bd5e3b9fa7d
Lemon-zeniye <endalklemon.abate@gmail.com>
2025-06-20 14:11:33 +0300

    lgoin page fixed

--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -72,12 +72,12 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="flex flex-col w-full mx-auto bg-transparent space-y-4 pt-10 px-4 max-h-screen">
+    <div className="flex flex-col w-full mx-auto bg-transparent space-y-4 pt-6  max-h-screen">
       {/* Header Section */}
       <div className="flex items-center justify-center gap-3">
         <img src={logo} alt="Akilo" className="w-24 h-24" />
         <div className="flex flex-col">
-          <p className="text-2xl font-bold bg-gradient-to-br from-black to-primary2 bg-clip-text text-transparent">
+          <p className="text-3xl font-bold bg-gradient-to-br from-black to-primary2 bg-clip-text text-transparent leading-10">
             Akilo Consultancy
             <br />
             Corporation
@@ -85,109 +85,111 @@ const LoginForm = () => {
         </div>
       </div>
 
-      <p className="text-xl text-center px-4 leading-7 font-medium">
-        Provide Technology Solutions to Elevate Your Business. Born in Tigray!
-      </p>
-
-      <button
-        className="bg-primary2 text-white flex items-center justify-center gap-2 w-full text-lg py-2 rounded-xl hover:bg-primary2/70"
-        onClick={handleClick}
-      >
-        Visit Akilo <MdArrowOutward className="text-xl" />
-      </button>
-
-      <div className="py-2 text-center">
-        <h2 className="text-xl">Wa'ela Check in Ticket</h2>
-        <p className="text-sm text-gray-600">
-          Login to access your Ticket Qr Code
+      <div className="px-4">
+        <p className="text-xl text-center px-4  font-medium leading-9">
+          Provide Technology Solutions to Elevate Your Business. Born in Tigray!
         </p>
-      </div>
 
-      {/* Login Form */}
-      <FormProvider {...methods}>
-        <form
-          className="flex flex-col w-full gap-y-4"
-          onSubmit={methods.handleSubmit(onSubmit)}
+        <button
+          className="bg-primary2 my-1 text-white flex items-center justify-center gap-2 w-full text-lg py-2 rounded-xl hover:bg-primary2/70"
+          onClick={handleClick}
         >
-          <FormField
-            control={methods.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input
-                    required
-                    type="text"
-                    placeholder="Enter your username"
-                    className="w-full !bg-white !py-3.5 rounded-lg"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+          Visit Akilo <MdArrowOutward className="text-xl" />
+        </button>
 
-          <FormField
-            control={methods.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <div className="relative">
+        <div className="py-2 text-center">
+          <h2 className="text-2xl">Wa'ela Check in Ticket</h2>
+          <p className="text-sm text-gray-600">
+            Login to access your Ticket Qr Code
+          </p>
+        </div>
+
+        {/* Login Form */}
+        <FormProvider {...methods}>
+          <form
+            className="flex flex-col w-full gap-y-4"
+            onSubmit={methods.handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={methods.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
                   <FormControl>
                     <Input
-                      type={showPassword ? "text" : "password"}
-                      placeholder="Enter your password"
-                      {...field}
                       required
-                      className="w-full pr-10 !bg-white !py-3.5 rounded-lg"
+                      type="text"
+                      placeholder="Enter your username"
+                      className="w-full !bg-white !py-3.5 rounded-lg"
+                      {...field}
                     />
                   </FormControl>
-                  <button
-                    type="button"
-                    className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
-                    onClick={() => setShowPassword(!showPassword)}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={methods.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <div className="relative">
+                    <FormControl>
+                      <Input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Enter your password"
+                        {...field}
+                        required
+                        className="w-full pr-10 !bg-white !py-3.5 rounded-lg"
+                      />
+                    </FormControl>
+                    <button
+                      type="button"
+                      className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? (
+                        <EyeOff className="h-5 w-5" />
+                      ) : (
+                        <Eye className="h-5 w-5" />
+                      )}
+                    </button>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={methods.control}
+              name="rememberMe"
+              render={({ field }) => (
+                <FormItem className="flex items-center space-x-2">
+                  <FormControl>
+                    <Checkbox {...field} id="terms" />
+                  </FormControl>
+                  <label
+                    htmlFor="terms"
+                    className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                   >
-                    {showPassword ? (
-                      <EyeOff className="h-5 w-5" />
-                    ) : (
-                      <Eye className="h-5 w-5" />
-                    )}
-                  </button>
-                </div>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                    Remember me
+                  </label>
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={methods.control}
-            name="rememberMe"
-            render={({ field }) => (
-              <FormItem className="flex items-center space-x-2">
-                <FormControl>
-                  <Checkbox {...field} id="terms" />
-                </FormControl>
-                <label
-                  htmlFor="terms"
-                  className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                >
-                  Remember me
-                </label>
-              </FormItem>
-            )}
-          />
-
-          <button
-            className="bg-primary2 text-white flex items-center justify-center gap-2 w-full text-lg py-3 rounded-xl hover:bg-primary2/70 mt-2"
-            type="submit"
-          >
-            {isLoading ? <Spinner /> : "Login"}
-          </button>
-        </form>
-      </FormProvider>
+            <button
+              className="bg-primary2 text-white flex items-center justify-center gap-2 w-full text-lg py-3 rounded-xl hover:bg-primary2/70 mt-2"
+              type="submit"
+            >
+              {isLoading ? <Spinner /> : "Login"}
+            </button>
+          </form>
+        </FormProvider>
+      </div>
     </div>
   );
 };
