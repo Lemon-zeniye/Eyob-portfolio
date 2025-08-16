e9047521cec675e614d643f656857a559fa13d5b
Lemon-zeniye <endalklemon.abate@gmail.com>
2025-08-16 21:59:52 +0300

    fix vedio playing

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -14,62 +14,62 @@ export type MediaGroup = {
 };
 
 // Example imports (replace paths with actual assets)
-import img1_ori from "../assets/img1_ori.jpg";
-import img1_ai_1 from "../assets/img1_ai_1.jpg";
-import img1_ai_2 from "../assets/img1_ai_2.jpg";
+import img1_ori from "/assets/img1_ori.jpg";
+import img1_ai_1 from "/assets/img1_ai_1.jpg";
+import img1_ai_2 from "/assets/img1_ai_2.jpg";
 
-import img2_ori from "../assets/img2_ori.jpg";
-import img2_ai_1 from "../assets/img2_ai_1.jpg";
-import img2_ai_2 from "../assets/img2_ai_2.jpg";
+import img2_ori from "/assets/img2_ori.jpg";
+import img2_ai_1 from "/assets/img2_ai_1.jpg";
+import img2_ai_2 from "/assets/img2_ai_2.jpg";
 
-import img3_ori from "../assets/img3_org.jpg";
-import vid3_ai from "../assets/vid3_ai.mp4";
+import img3_ori from "/assets/img3_org.jpg";
+import vid3_ai from "/assets/vid3_ai.mp4";
 
-import img4_ori from "../assets/img4_ori.jpg";
-import img4_ai from "../assets/img4_ai_1.jpg";
-import vid4_ai from "../assets/vid4_ai.mp4";
+import img4_ori from "/assets/img4_ori.jpg";
+import img4_ai from "/assets/img4_ai_1.jpg";
+import vid4_ai from "/assets/vid4_ai.mp4";
 
-import img5_ori from "../assets/img5_ori.jpg";
-import img5_ai_1 from "../assets/img5_ai_1.jpg";
-import vid5_ai from "../assets/vid5_ai.mp4";
+import img5_ori from "/assets/img5_ori.jpg";
+import img5_ai_1 from "/assets/img5_ai_1.jpg";
+import vid5_ai from "/assets/vid5_ai.mp4";
 
-import img6_ori from "../assets/img6_ori.jpg";
-import img6_ai from "../assets/img6_ai-1.jpg";
+import img6_ori from "/assets/img6_ori.jpg";
+import img6_ai from "/assets/img6_ai-1.jpg";
 
-import img7_ori from "../assets/img7_ori.jpg";
-import img7_ai from "../assets/img7_ai.jpg";
+import img7_ori from "/assets/img7_ori.jpg";
+import img7_ai from "/assets/img7_ai.jpg";
 
-import img8_ori from "../assets/img8_ori.jpg";
-import img8_ai from "../assets/img8_ai.jpg";
+import img8_ori from "/assets/img8_ori.jpg";
+import img8_ai from "/assets/img8_ai.jpg";
 
-import img9_ori from "../assets/img9_ori.jpg";
-import img9_ai from "../assets/img9_ai.jpg";
+import img9_ori from "/assets/img9_ori.jpg";
+import img9_ai from "/assets/img9_ai.jpg";
 
-import img10_ori from "../assets/img10_ori.jpg";
-import img10_ai from "../assets/img10_ai.jpg";
-import img10_ai_1 from "../assets/img10_ai_1.jpg";
+import img10_ori from "/assets/img10_ori.jpg";
+import img10_ai from "/assets/img10_ai.jpg";
+import img10_ai_1 from "/assets/img10_ai_1.jpg";
 
-import img11_ori from "../assets/img11_ori.jpg";
-import img11_ai_1 from "../assets/img11_ai_1.jpg";
-import img11_ai_2 from "../assets/img11_ai_2.jpg";
+import img11_ori from "/assets/img11_ori.jpg";
+import img11_ai_1 from "/assets/img11_ai_1.jpg";
+import img11_ai_2 from "/assets/img11_ai_2.jpg";
 
-import img12_ori from "../assets/img12_ori.jpg";
-import img12_ai_1 from "../assets/img12_ai_1.jpg";
-import img12_ai_2 from "../assets/img12_ai_2.jpg";
+import img12_ori from "/assets/img12_ori.jpg";
+import img12_ai_1 from "/assets/img12_ai_1.jpg";
+import img12_ai_2 from "/assets/img12_ai_2.jpg";
 
-import img13_ori from "../assets/img13_ori.jpg";
-import img13_ai from "../assets/img13_ai.jpg";
-import vid13 from "../assets/vid13_ai.mp4";
+import img13_ori from "/assets/img13_ori.jpg";
+import img13_ai from "/assets/img13_ai.jpg";
+import vid13 from "/assets/vid13_ai.mp4";
 
-import img14_ori from "../assets/img14_ori.jpg";
-import img14_ai from "../assets/img14_ai.png";
+import img14_ori from "/assets/img14_ori.jpg";
+import img14_ai from "/assets/img14_ai.png";
 
 // Random videos
-import r_vid_1 from "../assets/vid_1.mp4";
-import r_vid_2 from "../assets/vid_2.mp4";
-import r_vid_3 from "../assets/vid_3.mp4";
-import r_vid_4 from "../assets/vid_4.mp4";
-import r_vid_5 from "../assets/vid_5.mp4";
+import r_vid_1 from "/assets/vid_1.mp4";
+import r_vid_2 from "/assets/vid_2.mp4";
+import r_vid_3 from "/assets/vid_3.mp4";
+import r_vid_4 from "/assets/vid_4.mp4";
+import r_vid_5 from "/assets/vid_5.mp4";
 
 // Define the groups
 const groups: MediaGroup[] = [
@@ -213,6 +213,8 @@ const Gallery: React.FC = () => {
                 autoPlay
                 muted
                 loop
+                playsInline
+                preload="none"
                 className="w-full h-auto object-cover rounded-xl"
               />
             )}
